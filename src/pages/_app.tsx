878f6d99c3123e5299fefd2238463bc691a0fa5f
Syncxv <aruldevarackal1@gmail.com>
2022-03-04 01:45:36 +0530

    init gang

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import '../styles/main.scss'
+import type { AppProps } from 'next/app'
+import { ColorScheme, ColorSchemeProvider, MantineProvider } from '@mantine/core'
+import { useState } from 'react'
+
+function MyApp({ Component, pageProps }: AppProps) {
+    const [colorScheme, setColorScheme] = useState<ColorScheme>('light')
+    const toggleColorScheme = (value?: ColorScheme) =>
+        setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'))
+    return (
+        <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+            <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme: 'dark' }}>
+                <Component {...pageProps} />
+            </MantineProvider>
+        </ColorSchemeProvider>
+    )
+}
+
+export default MyApp
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Button, Center } from '@mantine/core'
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+    return (
+        <Center sx={{ height: '100vh' }}>
+            <Button color="green">Login with spotify eh</Button>
+        </Center>
+    )
+}
+
+export default Home
