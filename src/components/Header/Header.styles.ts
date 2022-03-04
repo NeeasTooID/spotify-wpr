62598ce50bfacd855dd50c7632a1ec63f4549946
Syncxv <aruldevarackal1@gmail.com>
2022-03-05 04:52:24 +0530

    header gang

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -1,16 +1,37 @@
 import { createStyles } from '@mantine/core'
+import { NAVBAR_WIDTH } from '../Sidebar/Sidebar.styles'
 
-export const HEADER_HEIGHT = 60
+export const HEADER_HEIGHT = 80
 export const HEADER_BREAKPOINT = 860
 
 export default createStyles(theme => ({
     header: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        position: 'fixed',
         top: 0,
         left: 0,
         right: 0,
         height: HEADER_HEIGHT,
         zIndex: 6,
-        position: 'fixed',
-        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.white
+        paddingRight: theme.spacing.lg,
+        paddingLeft: theme.spacing.lg,
+        marginLeft: NAVBAR_WIDTH
+    },
+    userPill: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: 'black',
+        borderRadius: theme.radius.xl,
+        padding: '.1em',
+        cursor: 'pointer'
+    },
+    image: {
+        borderRadius: '50%',
+        overflow: 'hidden',
+        height: '32px',
+        width: '32px'
     }
 }))
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,34 @@
-import { ScrollArea } from '@mantine/core'
+import { Box, Divider, Image, Menu, ScrollArea, Text } from '@mantine/core'
+import { Door, Gear, User } from 'phosphor-react'
 import React from 'react'
+import { zSpotifyStore } from '../../stores/spotify'
 import useStyles from './Header.styles'
 
 interface Props {}
 
 const Header: React.FC<Props> = props => {
     const { cx, classes } = useStyles()
+    const store = zSpotifyStore()
     return (
         <>
-            <header className={classes.header}>hi</header>
+            <header className={classes.header}>
+                <div></div>
+                <Menu
+                    control={
+                        <Box className={classes.userPill}>
+                            <Image
+                                className={classes.image}
+                                src={store.user?.images?.at(0)?.url ?? undefined}
+                            />
+                        </Box>
+                    }
+                >
+                    <Menu.Item icon={<User />}>Acount</Menu.Item>
+                    <Menu.Item icon={<Gear />}>Settings</Menu.Item>
+                    <Divider my={5} />
+                    <Menu.Item icon={<Door />}>Logout</Menu.Item>
+                </Menu>
+            </header>
         </>
     )
 }
