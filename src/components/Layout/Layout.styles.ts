dc7627b9e900262163da38a1cdb26e8e9ca3529c
Syncxv <aruldevarackal1@gmail.com>
2022-03-05 06:57:55 +0530

    idk prolly gonna change that color

--- src/components/Layout/Layout.styles.ts
+++ src/components/Layout/Layout.styles.ts
@@ -4,7 +4,18 @@ import { NAVBAR_WIDTH } from '../Sidebar/Sidebar.styles'
 const useStyles = createStyles(theme => ({
     main: {
         paddingLeft: NAVBAR_WIDTH,
-        paddingTop: HEADER_HEIGHT
+        paddingTop: HEADER_HEIGHT,
+        zIndex: '1',
+        '&::before': {
+            content: `''`,
+            position: 'absolute',
+            top: '0',
+            width: '100%',
+            height: '35%',
+            background: 'linear-gradient(to bottom, rgb(50 131 16) -26%, rgba(0, 0, 0, 0))',
+            filter: 'blur(1px)',
+            zIndex: '-1'
+        }
     }
 }))
 
