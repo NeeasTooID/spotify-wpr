f8bddcb1b69320d13ed7792d2574c9574523f880
Syncxv <aruldevarackal1@gmail.com>
2022-04-24 12:10:28 +0530

    oopsie

---
+++ src/pages/playlist/index.tsx
@@ -0,0 +1,12 @@
+import Layout from '../../components/Layout'
+
+interface Props {}
+
+const PlaylistMain: React.FC<Props> = () => {
+    return (
+        <Layout>
+            <div>PlaylistMain</div>
+        </Layout>
+    )
+}
+export default PlaylistMain
